bf9e18d6d69e0bb8b14c217d17ee113f3769ab81
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-17 17:28:37 +0100

    first commit

---
+++ js/scrypt.js
@@ -0,0 +1,15 @@
+/*
+CONSEGNA
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+*/
+
+/*
+1. Chiedere e salvare nome
+2. Chiedere e salvare cognome
+3. Chiedere e salvare colore preferito
+4. Salvare anno corrente
+5. Pubblicare nome, cognome, colore prerito e anno corrente
+*/
